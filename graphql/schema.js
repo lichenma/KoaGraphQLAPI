f4804b2571a624fccf36dbf7402f89d7f4126da2
Lichen Ma <lichenma123@gmail.com>
2019-04-15 17:27:40 -0400

    update

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,13 +1,19 @@
-const { buildSchema } = require('graphql');
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 const gadgetGraphQLType = require('./gadgetType');
 const Gadget = require('../models/gadget'); 
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        gadget: {
+            type: gadgetGraphQLType, 
+            args: { id: { type: GraphQLString }},
+            resolve(parent, args) {
+                return Gadget.findById(<a href="http://args.id" class="link link-url" target="_blank" rel="external nofollow noopener noreferrer">args.id</a>)
+            }
+        }
     }
-`);
+})
 
 
-module.exports = schema; 
+module.exports = new schema; 
